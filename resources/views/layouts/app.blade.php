741b3615197d05cda6f7b1e3ea8ed413b3cf51f7
Rober Diaz <rad8329@gmail.com>
2022-09-07 14:43:21 -0500

    Livewire and Tailwind was setup in the layout

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <title>{{ config('app.name') }}</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @vite('resources/css/tailwind.min.css')
+    @vite('resources/css/app.css')
+    @livewireStyles
+</head>
+    <body class="m-5 bg-white dark:bg-gray-900">
+        {{ $slot }}
+        @vite('resources/js/app.js')
+        @livewireScripts
+    </body>
+</html>
--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,9 @@ import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
     plugins: [
+
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ['resources/css/tailwind.min.css', 'resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
     ],
