87386a642572f65db1ad9f228407ee4effde3c95
Rober Diaz <rad8329@gmail.com>
2022-09-21 23:28:58 -0500

    Circuit breaker configuration

--- config/swapi.php
+++ config/swapi.php
@@ -3,5 +3,9 @@
 return [
     'url' => env('SWAPI_URL', 'https://swapi.dev/api/'),
     'cache_expiration' => env('SWAPI_CACHE_EXPIRATION_IN_SECONDS', 3600),
-    'timeout' => env('SWAPI_TIMEOUT_IN_SECONDS', 3)
+    'timeout' => env('SWAPI_TIMEOUT_IN_SECONDS', 3),
+    'circuit_beaker' => [
+        'threshold' => env('SWAPI_CIRCUIT_BREAKER_THRESHOLD', 3),
+        'time_to_live_in_seconds' => env('SWAPI_TIME_TO_LIVE_IN_SECONDS', 60),
+    ]
 ];
