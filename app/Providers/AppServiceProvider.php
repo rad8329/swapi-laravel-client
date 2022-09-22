48d9ab4368d093164f845bfbb5995cfb3a478511
Rober Diaz <rad8329@gmail.com>
2022-09-21 23:28:39 -0500

    Circuit breaker service binding

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,6 +6,8 @@ namespace App\Providers;
 
 use App\Services\Cache\ByMergingParametersResolver;
 use App\Services\Cache\CacheWithOptionsResolver;
+use App\Services\CircuitBreaker\Cache\CacheCircuitBreaker;
+use App\Services\CircuitBreaker\CircuitBreakerInterface;
 use App\Services\Hashing\ByJsonEncodedHasher;
 use App\Services\Hashing\Hasher;
 use Illuminate\Support\ServiceProvider;
@@ -19,5 +21,6 @@ class AppServiceProvider extends ServiceProvider
     {
         $this->app->bind(Hasher::class, ByJsonEncodedHasher::class);
         $this->app->bind(CacheWithOptionsResolver::class, ByMergingParametersResolver::class);
+        $this->app->bind(CircuitBreakerInterface::class, CacheCircuitBreaker::class);
     }
 }
